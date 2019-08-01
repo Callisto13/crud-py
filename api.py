6bba20c9899b02abae34208ee75a8e1a8a803412
Claudia Beresford <cberesford@pivotal.io>
2019-08-01 16:33:42 +0100

    add create endpoint

--- api.py
+++ api.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, request
+import sys
 
 app = Flask(__name__)
+
+app.config['store'] = sys.argv[1]
+store = app.config.get('store')
 
 @app.route('/')
 def index():
     return "welcome home"
+
+@app.route('/files/create', methods=['POST'])
+def create():
+    data = request.get_json()
+    name, contents = data['name'], data['contents']
+
+    f = open(store+'/'+name, "w")
+    f.write(contents)
+    f.close()
+
+    return "File '{}' created at '{}'.".format(name, store), 201
+
 
 app.run(debug=True)
--- api_test.py
+++ api_test.py
@@ -1,17 +1,27 @@
 import requests
 import pexpect
+import tempfile
+import shutil
+import sys
 
 import pytest
 
 class TestAPI():
 
     @pytest.fixture(autouse=True)
-    def setup(self):
+    def setup(self, tmp_dir):
         self.url = 'http://127.0.0.1:5000'
+        self.tmp = tmp_dir
+
+    @pytest.fixture()
+    def tmp_dir(self):
+        tmp = tempfile.mkdtemp(prefix = "pcrud")
+        yield tmp
+        shutil.rmtree(tmp)
 
     @pytest.fixture(autouse=True)
     def start_server(self):
-        server = pexpect.spawn("python api.py")
+        server = pexpect.spawn("python api.py "+self.tmp)
         server.expect('Running on '+self.url)
         yield server
         server.kill(9)
@@ -19,4 +29,17 @@ class TestAPI():
     def test_index(self):
         r = requests.get(self.url)
         assert r.status_code == 200
+
+    def test_post_create(self):
+        content_header = {'Content-Type': 'application/json'}
+        data = {'name': 'test-file', 'contents':'hello'}
+        r = requests.post(self.url+"/files/create", headers=content_header, json=data)
+
+        assert r.status_code == 201
+        assert r.text == "File 'test-file' created at '"+self.tmp+"'."
+
+        file_object = open(self.tmp+"/test-file", "r")
+        read_content = file_object.read()
+        file_object.close()
+        assert read_content == "hello"
 
